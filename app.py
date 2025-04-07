4fa346f9cea37d2d338cf42841b7996f0539b094
Beyza <cicekaybeyza@gmail.com>
2025-04-08 00:18:21 +0300

    bakalım jenkins çalışıyor mu

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ PORT = 80
 handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Sunucu başlatıldı: http://localhost:80")
+    print("Sunucu başlatıldıııııı: http://localhost:80")
     httpd.serve_forever()
