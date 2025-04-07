9e434d2324deddd61d8bafa77d7ba3cc8a1c644d
Beyza <cicekaybeyza@gmail.com>
2025-04-07 23:58:54 +0300

    helal

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("merhaba docker")
+import http.server
+import socketserver
+
+PORT = 80
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print("Sunucu başlatıldı: http://localhost:80")
+    httpd.serve_forever()
